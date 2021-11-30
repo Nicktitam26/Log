45f1aa769388098c2ce63f10bee8078fc36572b7
Nick <Nick@gmail.com>
2021-11-30 14:36:27 -0500

    Log Completo

--- src/Log.java
+++ src/Log.java
@@ -12,4 +12,39 @@ public class Log {
     private BufferedWriter buffered;
     private String ruta;
 
+    public Log(String ruta) throws IOException {
+        this.ruta = ruta;
+        this.open(true);
+    }
+
+    public Log(String ruta, boolean reset) throws IOException {
+        this.ruta = ruta;
+        this.open(!reset);
+    }
+
+    private void open(boolean b) {
+    }
+
+    public String[] getLines() throws FileNotFoundException, IOException {  
+    ArrayList <String> linesFile = new ArrayList<>();
+    
+    BufferedReader br = new BufferedReader(new FileReader(this.ruta));
+
+    String line;
+    while ((line = br.readLine()) != null) {
+        linesFile.add(line);
+    }
+
+    br.close();
+
+    String[] lines = new String[linesFile.size()];
+
+    for (int i = 0; i < linesFile.size(); i++) {
+        lines[i] = linesFile.get(i);
+    }
+
+    return lines;
+    }
 }
+    
+
