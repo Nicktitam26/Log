1fe0c15fb1e7c29c2ffe74a8d72a881909eae8e8
CrlsDev <c6099020@gmail.com>
2021-11-30 14:35:42 -0500

    Funciones para manejar el log

--- src/Log.java
+++ src/Log.java
@@ -11,5 +11,21 @@ import java.util.Date;
 public class Log {
     private BufferedWriter buffered;
     private String ruta;
-
+    private void open(boolean append) throws IOException{
+        this.buffered = new BufferedWriter(new FileWriter(this.ruta,append));
+    }
+    public void addLine(String line) throws IOException{
+        SimpleDateFormat DateFormat = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
+        String Fecha = DateFormat.format(new Date());
+        this.open(true);
+        this.buffered.write("["+Fecha+"]" + line + "\n");
+        this.close();
+    }
+    public void resetLog() throws IOException {
+        this.open(false);
+        this.close();
+    }
+    private void close() throws IOException{
+        this.buffered.close();
+    }
 }
