d8babf4fedaa937e877d4c8cfb52565d39e7b9e8
Nick <Nick@gmail.com>
2021-11-30 14:16:59 -0500

    Creación de la clase log

---
+++ src/Log.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Log {
+    private BufferedWriter buffered;
+    private string ruta;
+    
+}
