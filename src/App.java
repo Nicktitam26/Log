db993ca31e0de07c0ef06fc6de960f5faef6350a
CrlsDev <c6099020@gmail.com>
2021-11-30 13:39:16 -0500

    Reinicio del proyecto

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("HOLA Q HACE");
-        System.out.println("Hola desde mi pc");
+
     }
 }
